b42975eb85d39e069d81e1262f04ab00eb672728
meiqiang <144363600@qq.com>
2020-07-01 20:01:20 +0800

    自营管理模块

--- src/pages/self/SelfOrder.vue
+++ src/pages/self/SelfOrder.vue
@@ -1,20 +1,20 @@
 <template>
   <!-- 自营订单管理 -->
-  <div class="content">
+  <div class="content order">
     <!-- 搜索区域 -->
     <el-form :inline="true" :model="searchForm" class="searchForm">
       <el-form-item label="订单号：">
-        <el-input v-model="searchForm.keyword" placeholder="请输入订单号搜索"></el-input>
+        <el-input v-model="searchForm.code" placeholder="请输入订单号搜索"></el-input>
       </el-form-item>
       <el-form-item label="买家名称：">
-        <el-input v-model="searchForm.keyword" placeholder="请输入买家名称搜索"></el-input>
+        <el-input v-model="searchForm.buyer" placeholder="请输入买家名称搜索"></el-input>
       </el-form-item>
       <el-form-item label="商品名称：">
-        <el-input v-model="searchForm.keyword" placeholder="请输入商品名称搜索"></el-input>
+        <el-input v-model="searchForm.productTitle" placeholder="请输入商品名称搜索"></el-input>
       </el-form-item>
       <el-form-item label="下单时间：">
         <el-date-picker
-          v-model="searchForm.dates[0]"
+          v-model="searchForm.dateStart"
           type="datetime"
           placeholder="选择起始日期"
           @change="startChange"
@@ -22,7 +22,7 @@
       </el-form-item>
       <el-form-item>
         <el-date-picker
-          v-model="searchForm.dates[1]"
+          v-model="searchForm.dateEnd"
           type="datetime"
           placeholder="选择结束日期"
           @change="endChange"
@@ -33,49 +33,191 @@
       </el-form-item>
     </el-form>
 
-    <el-tabs type="border-card">
-      <el-tab-pane label="全部订单"></el-tab-pane>
-      <el-tab-pane label="待处理(8)"></el-tab-pane>
-      <el-tab-pane label="待卖家确认(0)"></el-tab-pane>
-      <el-tab-pane label="待发货(0)"></el-tab-pane>
-      <el-tab-pane label="待收货(0)"></el-tab-pane>
-      <el-tab-pane label="已完成(0)"></el-tab-pane>
-      <el-table :data="memberData" border style="width: 100%;">
-        <el-table-column type="index" label="序号" fixed></el-table-column>
-        <el-table-column prop="code" label="供应商编号" width="190"></el-table-column>
-        <el-table-column prop="name" label="供应商名称" width="190" show-overflow-tooltip></el-table-column>
-        <el-table-column prop="mobile" label="手机" width="180"></el-table-column>
-        <el-table-column prop label="地区" width="180">
-          <template slot-scope="scope">
-            <span>
-              {{ scope.row.supplierCompanyOutput.province
-              }}{{ scope.row.supplierCompanyOutput.city }}
-            </span>
-          </template>
-        </el-table-column>
-        <el-table-column label="状态" width="150">
-          <template slot-scope="scope">
-            <span>{{ scope.row.status.text }}</span>
-          </template>
-        </el-table-column>
-        <el-table-column label="金牌供应商" width="110">
-          <template slot-scope="scope">
-            <span v-if="scope.row.vip == 0">否</span>
-            <span v-if="scope.row.vip == 1">是</span>
-          </template>
-        </el-table-column>
-        <el-table-column label="金牌时效周期" width="260">
-          <template slot-scope="scope">
-            <span v-if="!scope.row.vipDate || !scope.row.vipExpired"></span>
-            <span v-else>
-              {{
-              scope.row.vipDate + " - " + scope.row.vipExpired
-              }}
-            </span>
-          </template>
-        </el-table-column>
-        <el-table-column prop="createDate" label="注册时间" sortablewidth="170"></el-table-column>
-      </el-table>
+    <el-tabs type="border-card" v-model="activeName" @tab-click="handleClick">
+      <el-tab-pane label="全部订单" name></el-tab-pane>
+      <el-tab-pane label="待处理(8)" name></el-tab-pane>
+      <el-tab-pane label="待卖家确认(0)" name></el-tab-pane>
+      <el-tab-pane label="待发货(0)" name></el-tab-pane>
+      <el-tab-pane label="待收货(0)" name></el-tab-pane>
+      <el-tab-pane label="已完成(0)" name></el-tab-pane>
+      <div class="order_title">
+        <el-row>
+          <el-col :span="6">
+            <div class="grid-content bg-purple">商品信息</div>
+          </el-col>
+          <el-col :span="3">
+            <div class="grid-content bg-purple-light">单价</div>
+          </el-col>
+          <el-col :span="3">
+            <div class="grid-content bg-purple">数量</div>
+          </el-col>
+          <el-col :span="3">
+            <div class="grid-content bg-purple-light">总金额</div>
+          </el-col>
+          <el-col :span="3">
+            <div class="grid-content bg-purple-light">确认金额</div>
+          </el-col>
+          <el-col :span="3">
+            <div class="grid-content bg-purple-light">订单状态</div>
+          </el-col>
+          <el-col :span="3">
+            <div class="grid-content bg-purple-light">操作</div>
+          </el-col>
+        </el-row>
+      </div>
+      <div class="item">
+        <div class="item_hd">
+          <el-row>
+            <el-col :span="6">
+              <div class="grid-content bg-purple code">订单编号：SO15893606901730</div>
+            </el-col>
+            <el-col :span="6">
+              <div class="grid-content bg-purple-light time">下单时间：2020-5-15 16:33:12</div>
+            </el-col>
+            <el-col :span="6">
+              <div class="grid-content bg-purple user">买家用户：linzehao123</div>
+            </el-col>
+            <el-col :span="6">
+              <div class="grid-content bg-purple-light detail">
+                <el-link type="primary">订单详情</el-link>
+              </div>
+            </el-col>
+          </el-row>
+        </div>
+        <div class="item_bd">
+          <el-row>
+            <el-col :span="6">
+              <div class="grid-content bg-purple avatar">
+                <el-image style="width: 50px; height: 50px" :src="url"></el-image>
+                <span class="tit">Raxwell 直柄麻花钻，2mm，10支Raxwell 直柄麻花钻，2mm，10支Raxwell 直柄麻花钻，2mm，10支</span>
+              </div>
+            </el-col>
+            <el-col :span="3">
+              <div class="grid-content bg-purple-light">¥17500</div>
+            </el-col>
+            <el-col :span="3">
+              <div class="grid-content bg-purple">1</div>
+            </el-col>
+            <el-col :span="3">
+              <div class="grid-content bg-purple-light">¥17500</div>
+            </el-col>
+            <el-col :span="3">
+              <div class="grid-content bg-purple-light">待确认</div>
+            </el-col>
+            <el-col :span="3">
+              <div class="grid-content bg-purple-light">待商家确认</div>
+            </el-col>
+            <el-col :span="3">
+              <div class="grid-content bg-purple-light handle">
+                <span>修改价格</span>
+                <span>取消订单</span>
+              </div>
+            </el-col>
+          </el-row>
+        </div>
+      </div>
+      <div class="item">
+        <div class="item_hd">
+          <el-row>
+            <el-col :span="6">
+              <div class="grid-content bg-purple code">订单编号：SO15893606901730</div>
+            </el-col>
+            <el-col :span="6">
+              <div class="grid-content bg-purple-light time">下单时间：2020-5-15 16:33:12</div>
+            </el-col>
+            <el-col :span="6">
+              <div class="grid-content bg-purple user">买家用户：linzehao123</div>
+            </el-col>
+            <el-col :span="6">
+              <div class="grid-content bg-purple-light detail">
+                <el-link type="primary">订单详情</el-link>
+              </div>
+            </el-col>
+          </el-row>
+        </div>
+        <div class="item_bd">
+          <el-row>
+            <el-col :span="6">
+              <div class="grid-content bg-purple avatar">
+                <el-image style="width: 50px; height: 50px" :src="url"></el-image>
+                <span>Raxwell 直柄麻花钻，2mm，10支</span>
+              </div>
+            </el-col>
+            <el-col :span="3">
+              <div class="grid-content bg-purple-light">¥17500</div>
+            </el-col>
+            <el-col :span="3">
+              <div class="grid-content bg-purple">1</div>
+            </el-col>
+            <el-col :span="3">
+              <div class="grid-content bg-purple-light">¥17500</div>
+            </el-col>
+            <el-col :span="3">
+              <div class="grid-content bg-purple-light">待确认</div>
+            </el-col>
+            <el-col :span="3">
+              <div class="grid-content bg-purple-light">待商家确认</div>
+            </el-col>
+            <el-col :span="3">
+              <div class="grid-content bg-purple-light handle">
+                <span>修改价格</span>
+                <span>取消订单</span>
+              </div>
+            </el-col>
+          </el-row>
+        </div>
+      </div>
+      <div class="item">
+        <div class="item_hd">
+          <el-row>
+            <el-col :span="6">
+              <div class="grid-content bg-purple code">订单编号：SO15893606901730</div>
+            </el-col>
+            <el-col :span="6">
+              <div class="grid-content bg-purple-light time">下单时间：2020-5-15 16:33:12</div>
+            </el-col>
+            <el-col :span="6">
+              <div class="grid-content bg-purple user">买家用户：linzehao123</div>
+            </el-col>
+            <el-col :span="6">
+              <div class="grid-content bg-purple-light detail">
+                <el-link type="primary">订单详情</el-link>
+              </div>
+            </el-col>
+          </el-row>
+        </div>
+        <div class="item_bd">
+          <el-row>
+            <el-col :span="6">
+              <div class="grid-content bg-purple avatar">
+                <el-image style="width: 50px; height: 50px" :src="url"></el-image>
+                <span>Raxwell 直柄麻花钻，2mm，10支</span>
+              </div>
+            </el-col>
+            <el-col :span="3">
+              <div class="grid-content bg-purple-light">¥17500</div>
+            </el-col>
+            <el-col :span="3">
+              <div class="grid-content bg-purple">1</div>
+            </el-col>
+            <el-col :span="3">
+              <div class="grid-content bg-purple-light">¥17500</div>
+            </el-col>
+            <el-col :span="3">
+              <div class="grid-content bg-purple-light">待确认</div>
+            </el-col>
+            <el-col :span="3">
+              <div class="grid-content bg-purple-light">待商家确认</div>
+            </el-col>
+            <el-col :span="3">
+              <div class="grid-content bg-purple-light handle">
+                <span>修改价格</span>
+                <span>取消订单</span>
+              </div>
+            </el-col>
+          </el-row>
+        </div>
+      </div>
     </el-tabs>
 
     <!-- 分页区域 -->
@@ -100,35 +242,34 @@ export default {
     return {
       // 搜索表单
       searchForm: {
-        keyword: '',
-        status: '',
-        token: token || '',
-        vip: '',
+        code: '', // 订单编号
+        buyer: '', // 买家
+        productTitle: '', // 产品名称
+        status: '', // 订单状态
+        dateStart: '', // 起始日期
+        dateEnd: '', // 结束日期
         page: 0,
-        size: 20,
-        dates: [] // 起止时间
+        size: 20
       },
       total: null,
-      memberData: [], // 列表数据
-      // 设置金牌对话框数据
-      isShowGlodDialog: false,
-      glodForm: {
-        dates: []
-      },
-      id: null // 当前id
+      orderData: [], // 订单数据
+      activeName: '',
+      id: null, // 当前id
+      url:
+        'https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg'
     }
   },
   created() {
-    this.getApplyList()
+    this.getOrderList()
   },
   methods: {
-    getApplyList() {
+    getOrderList() {
       this.axios
-        .post(`${this.baseUrl}/api/supplier/search`, this.searchForm)
+        .get(`${this.baseUrl}/api/admin/self/goodsOrderPage`, this.searchForm)
         .then(res => {
           console.log(res)
           if (res.code == 200) {
-            this.memberData = res.data.content
+            this.orderData = res.data.content
             this.searchForm.page = res.data.number
             this.searchForm.size = res.data.size
             this.total = res.data.totalElements
@@ -136,47 +277,37 @@ export default {
         })
     },
     startChange() {
-      if (this.searchForm.dates.length > 0)
-        this.searchForm.dates[0] = this.$timeDate(this.searchForm.dates[0])
-      if (this.glodForm.dates.length > 0)
-        this.glodForm.dates[0] = this.$timeDate(this.glodForm.dates[0])
+      this.searchForm.dateStart = this.$timeDate(this.searchForm.dateStart)
     },
     endChange() {
-      if (this.searchForm.dates.length > 0)
-        this.searchForm.dates[1] = this.$timeDate(this.searchForm.dates[1])
-      if (this.glodForm.dates.length > 0)
-        this.glodForm.dates[1] = this.$timeDate(this.glodForm.dates[1])
+      this.searchForm.dateEnd = this.$timeDate(this.searchForm.dateEnd)
     },
     // 查询
     query() {
       this.searchForm.page = 0
       if (
-        new Date(this.searchForm.dates[0]).getTime() >
-        new Date(this.searchForm.dates[1]).getTime()
+        new Date(this.searchForm.dateStart).getTime() >
+        new Date(this.searchForm.dateEnd).getTime()
       ) {
         return this.$message.warning('起始时间不能大于结束时间！')
       }
-      this.getApplyList()
+      this.getOrderList()
     },
     handleSizeChange(val) {
       this.searchForm.size = val
-      this.getApplyList()
+      this.getOrderList()
     },
     handleCurrentChange(val) {
       this.searchForm.page = val
-      this.getApplyList()
+      this.getOrderList()
+    },
+    handleClick(tab, event) {
+      console.log(tab, event)
     }
   }
 }
 </script>
 
 <style>
-.glodDialog .el-dialog {
-  width: 600px !important;
-  text-align: center;
-}
-.glodDialog .el-dialog__body {
-  padding-left: 30px;
-}
 </style>
 
